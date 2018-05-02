df3d2844aae72757096f8e7077d2050ef317f72f
Russell Baker <rbakerjax@gmail.com>
2018-05-02 14:48:35 -0400

    Better navigation

--- lib/pantry_web/controllers/session_controller.ex
+++ lib/pantry_web/controllers/session_controller.ex
@@ -10,7 +10,7 @@ defmodule PantryWeb.SessionController do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcom back!")
-        |> redirect(to: page_path(conn, :index))
+        |> redirect(to: client_path(conn, :index))
 
       {:error, _reason, conn} ->
         conn
