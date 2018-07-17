4f80e990fd0daada41fbfece750247a0438595df
Russell Baker <rbakerjax@gmail.com>
2018-07-17 11:39:03 -0400

    Signature Pad is installed and working.

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,7 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 import "bootstrap"
+import "signature_pad"
 
 // Import local files
 //
@@ -20,3 +21,24 @@ import "bootstrap"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+//
+//
+//
+
+var signaturePad = new SignaturePad(document.getElementById('signature-pad'), {
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  penColor: 'rgb(0, 0, 0)'
+});
+var saveButton = document.getElementById('save');
+var cancelButton = document.getElementById('clear');
+
+saveButton.addEventListener('click', function (event) {
+  var data = signaturePad.toDataURL('image/png');
+
+// Send data to server instead...
+  window.open(data);
+});
+
+cancelButton.addEventListener('click', function (event) {
+  signaturePad.clear();
+});
