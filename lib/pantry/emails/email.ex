681c04a93872ab46abc3af3926799d64196a3742
Russell Baker <rbakerjax@gmail.com>
2018-09-02 20:06:11 -0400

    Successfully sending emails locally

--- lib/pantry/emails/email.ex
+++ lib/pantry/emails/email.ex
@@ -1,3 +1,13 @@
 defmodule Pantry.Emails.Email do
   import Bamboo.Email
+
+  def reset_password_email do
+    new_email(
+      to: "rbakerjax@gmail.com",
+      from: "pantry.russellbaker.me",
+      subject: "you forgot it",
+      html_body: "<strong>Thanks for forgetting!</strong>",
+      text_body: "here is your password reset"
+    )
+  end
 end
